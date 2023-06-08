93d94b4fc1de74cff3990e8d12f76ac04482e042
Dillon <dillonkyle95@protonmail.com>
2023-06-08 01:09:33 -0700

    Update firebase.js

--- exchange/src/firebase.js
+++ exchange/src/firebase.js
@@ -8,13 +8,11 @@ import firebase from "firebase";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDWMT_BC06jEnk7o1vb5PtczAY6xqCHygA",
-  authDomain: "exchange-97475.firebaseapp.com",
-  projectId: "exchange-97475",
-  storageBucket: "exchange-97475.appspot.com",
-  messagingSenderId: "1035982071687",
-  appId: "1:1035982071687:web:6e5d7de0cd8fae1675d522",
-  measurementId: "G-1KK749J65C"
+  
+ 
+  
+
+ 
 };
 
 // Initialize Firebase
@@ -23,6 +21,3 @@ const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 // const analytics = getAnalytics(app);
-
-export { auth, provider };
-export default db;
